d65fa89b512644059016d6684d596a5f9101232f
juliemturner <julie.turner@sympraxisconsulting.com>
2017-05-31 16:55:10 -0400

    Update for modifications to graph api

--- MSGraph ADAL Intro/SPADALMSGraphAPI/client/js/module.adal.js
+++ MSGraph ADAL Intro/SPADALMSGraphAPI/client/js/module.adal.js
@@ -6,15 +6,16 @@ var GraphExcel;
             enabled: true,
             requireBase: false
         }).hashPrefix('!');
+
         adalProvider.init({
-            tenant: '<your tenant id>',
-            clientId: '<your client id>',
+            tenant: '<tenant guid>',
+            clientId: '<application id>',
             endpoints: {
                 'https://graph.microsoft.com': 'https://graph.microsoft.com'
             },
-            cacheLocation: "localStorage",
+            cacheLocation: "sessionStorage",
             //endpoints you want ADAL to ignore, they are inclusive paths, also you must use relative paths, if you include http/https it will look for a resource and automatically append the token of the loginResource
-            anonymousEndpoints: ['<your templates folder location>', '<your site collection>/_api/']
+            anonymousEndpoints: ['/sites/juliedemos', '/sites/juliedemos/_api/']
         }, $httpProvider);
     }
     //Module dependencies
@@ -25,6 +26,6 @@ var GraphExcel;
         .config(configure)
         .constant("_CONFIG", {
         "ONEDRIVE_EP": "https://graph.microsoft.com/v1.0/me/drive/", //if you want to try it against onedrive
-        "SP_EP": "https://graph.microsoft.com/beta/sharepoint/sites/<site guid>/lists/<list guid>/drive/"
+        "SP_EP": "https://graph.microsoft.com/beta/sites/<contoso.sharepoint.com>,<_api/site/id>,<_api/site/rootweb/id>/lists/<list guid>/drive/"
     });
 })(GraphExcel || (GraphExcel = {}));
